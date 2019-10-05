fdaed4ac4c0a2537f6b0b931a538e6442f773ff4
Ivan Nazarov <ivannz@yandex.ru>
2019-10-05 21:07:55 +0300

    Update MRO in SVDD to satisfy #14884

--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -1457,7 +1457,7 @@ class OneClassSVM(OutlierMixin, BaseLibSVM):
         }
 
 
-class SVDD(BaseLibSVM, OutlierMixin):
+class SVDD(OutlierMixin, BaseLibSVM):
     """Support Vector Data Description (SVDD) for Unsupervised Outlier Detection.
 
     Estimate the support of a high-dimensional distribution by finding the
