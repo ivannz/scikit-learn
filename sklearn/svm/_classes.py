f32a715784b4f39219b00ef56f6cb40fbcb4e433
ivannz <ivannz@yandex.ru>
2020-01-30 23:08:36 +0300

    some oneliners

--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -1627,8 +1627,7 @@ class SVDD(OutlierMixin, BaseLibSVM):
         dec : ndarray of shape (n_samples,)
             Returns the decision function of the samples.
         """
-        dec = self._decision_function(X).ravel()
-        return dec
+        return self._decision_function(X).ravel()
 
     def score_samples(self, X):
         """Raw scoring function of the samples.
@@ -1643,8 +1642,7 @@ class SVDD(OutlierMixin, BaseLibSVM):
         score_samples : ndarray of shape (n_samples,)
             Returns the (unshifted) scoring function of the samples.
         """
-        score_samples = self.decision_function(X) + self.offset_
-        return score_samples
+        return self.decision_function(X) + self.offset_
 
     def predict(self, X):
         """Perform classification on samples in X.
